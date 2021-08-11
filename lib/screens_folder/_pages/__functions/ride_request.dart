dd68c9efbaa89ae84ce3ce30d9e9afe75dee50b8
iiTzMontzz <ritchie.monterola16@gmail.com>
2021-08-11 15:44:12 +0800

    Aug 11

--- lib/screens_folder/_pages/__functions/ride_request.dart
+++ lib/screens_folder/_pages/__functions/ride_request.dart
@@ -1,13 +1,16 @@
 import 'dart:async';
+import 'package:ff_user/models_folder/direction_details.dart';
 import 'package:ff_user/screens_folder/_pages/__functions/_petshop/pet_shop_search.dart';
 import 'package:ff_user/screens_folder/_pages/__functions/_user/user_search.dart';
 import 'package:ff_user/screens_folder/_pages/__functions/_vet/vet_search.dart';
 import 'package:ff_user/services_folder/_database/app_data.dart';
 import 'package:ff_user/services_folder/_helper/helper_method.dart';
 import 'package:ff_user/shared_folder/_buttons/divider.dart';
+import 'package:ff_user/shared_folder/_constants/progressDialog.dart';
 import 'package:ff_user/shared_folder/_constants/size_config.dart';
 import 'package:ff_user/shared_folder/_global/glob_var.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
@@ -25,6 +28,11 @@ class _RideRequestState extends State<RideRequest> {
   Completer<GoogleMapController> _controller = Completer();
   GoogleMapController mapController;
   Position currentPosition;
+  DirectionDetails tripDirectionDetails;
+  List<LatLng> polylineCoordinates = [];
+  Set<Polyline> _polylines = {};
+  Set<Marker> _markers = {};
+  Set<Circle> _circles = {};
   double mapPadding = 0;
   double screensheet = 285;
   bool showTopnavi = true;
@@ -44,6 +52,9 @@ class _RideRequestState extends State<RideRequest> {
             myLocationEnabled: true,
             zoomControlsEnabled: true,
             zoomGesturesEnabled: true,
+            polylines: _polylines,
+            markers: _markers,
+            circles: _circles,
             onMapCreated: onMapCreated,
           ),
 
@@ -134,8 +145,13 @@ class _RideRequestState extends State<RideRequest> {
                     SizedBox(height: getProportionateScreenHeight(20)),
                     GestureDetector(
                       onTap: () async {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (BuildContext context) => UserSearch()));
+                        var response = await Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    UserSearch()));
+                        if (response == 'getDirections') {
+                          await getDirections();
+                        }
                       },
                       child: Container(
                         decoration: BoxDecoration(
@@ -291,5 +307,106 @@ class _RideRequestState extends State<RideRequest> {
 
     var pickupLatLng = LatLng(pickUp.lat, pickUp.lng);
     var destinationLatLng = LatLng(destination.lat, destination.lng);
+
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) =>
+            ProgressDialog(status: 'Getting Directions.....'));
+    var thisDetails =
+        await HelperMethod.getDirectionDetails(pickupLatLng, destinationLatLng);
+    setState(() {
+      tripDirectionDetails = thisDetails;
+    });
+    Navigator.of(context).pop();
+    PolylinePoints polylinepoints = PolylinePoints();
+    List<PointLatLng> result =
+        polylinepoints.decodePolyline(thisDetails.encodedPoints);
+    polylineCoordinates.clear();
+    if (result.isNotEmpty) {
+      //Looping through the encoded points to convert into polyline points
+      result.forEach((PointLatLng points) {
+        polylineCoordinates.add(LatLng(points.latitude, points.longitude));
+      });
+    }
+    _polylines.clear();
+    setState(() {
+      Polyline polyline = Polyline(
+        polylineId: PolylineId('polyID'),
+        color: Colors.blueAccent[700],
+        points: polylineCoordinates,
+        jointType: JointType.round,
+        width: 4,
+        startCap: Cap.buttCap,
+        endCap: Cap.roundCap,
+        geodesic: true,
+      );
+      _polylines.add(polyline);
+    });
+    //Fitting polyline points in the map after picking a destination adrress
+    LatLngBounds bounds;
+    if (pickupLatLng.latitude > destinationLatLng.latitude &&
+        pickupLatLng.longitude > destinationLatLng.longitude) {
+      bounds =
+          LatLngBounds(southwest: destinationLatLng, northeast: pickupLatLng);
+    } else if (pickupLatLng.longitude > destinationLatLng.longitude &&
+        pickupLatLng.longitude > destinationLatLng.longitude) {
+      bounds = LatLngBounds(
+        southwest: LatLng(pickupLatLng.latitude, destinationLatLng.longitude),
+        northeast: LatLng(destinationLatLng.latitude, pickupLatLng.longitude),
+      );
+    } else if (pickupLatLng.latitude > destinationLatLng.latitude) {
+      bounds = LatLngBounds(
+        southwest: LatLng(destinationLatLng.latitude, pickupLatLng.longitude),
+        northeast: LatLng(pickupLatLng.latitude, destinationLatLng.longitude),
+      );
+    } else {
+      bounds = LatLngBounds(
+        southwest: pickupLatLng,
+        northeast: destinationLatLng,
+      );
+    }
+    mapController.animateCamera(CameraUpdate.newLatLngBounds(bounds, 70));
+    //Markers
+    Marker pickupMarker = Marker(
+      markerId: MarkerId('pickup'),
+      position: pickupLatLng,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+      infoWindow: InfoWindow(title: pickUp.placename, snippet: 'My Location'),
+    );
+    Marker destinationMarker = Marker(
+      markerId: MarkerId('destination'),
+      position: destinationLatLng,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      infoWindow: InfoWindow(
+          title: destination.placename, snippet: 'Destination Location'),
+    );
+
+    setState(() {
+      _markers.add(pickupMarker);
+      _markers.add(destinationMarker);
+    });
+    //Cricles
+    Circle pickupCircle = Circle(
+      circleId: CircleId('pickup'),
+      strokeColor: Colors.green,
+      strokeWidth: 3,
+      radius: 12,
+      center: pickupLatLng,
+      fillColor: Colors.green,
+    );
+    Circle destinationCircle = Circle(
+      circleId: CircleId('destination'),
+      strokeColor: Colors.purpleAccent,
+      strokeWidth: 3,
+      radius: 12,
+      center: destinationLatLng,
+      fillColor: Colors.purpleAccent,
+    );
+
+    setState(() {
+      _circles.add(pickupCircle);
+      _circles.add(destinationCircle);
+    });
   }
 }
